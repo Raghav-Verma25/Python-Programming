5252fe16033dfaacaf18185ae7aa198d96723854
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-01-17 19:31:19 +0530

    Chicken&Goat.py

---
+++ Chicken&Goat.py
@@ -0,0 +1,11 @@
+h=int(input("enter number of head in farm="))
+g=int(input("enter a number of feets in farm"))
+a="chicken"
+b="goat"
+"""a+b=h
+2a+4b=g"""
+b=(g-(2*h))/2
+a=h-b
+
+print("no. of chickens =",a)
+print("no. of goats =",b)
